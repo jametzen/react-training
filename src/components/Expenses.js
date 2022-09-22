c9a671bfc5204d735cd75646a2d2a59868cfb5df
Jeremiah Metzen <jmetzen@colinsgrp.com>
2022-09-22 11:23:46 -0500

    Section 3 through assignment.

---
+++ src/components/Expenses.js
@@ -0,0 +1,17 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses(props){
+
+    return(
+        <div className="expenses">
+            {
+                props.expenses.map(expense => {
+                    return <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}></ExpenseItem>
+                })
+            }
+        </div>
+    );
+}
+
+export default Expenses;
